3d64d88a00f42b8bd62425ed915654e893ce7020
Hawk Weisman <hi@hawkweisman.me>
2015-05-20 13:12:42 -0400

    Fix fallout from moving Random to its own package

--- src/test/scala/UtilSpec.scala
+++ src/test/scala/UtilSpec.scala
@@ -16,25 +16,25 @@ class UtilSpec extends FlatSpec with GeneratorDrivenPropertyChecks with Matchers
 
   "The randomAlphanumericString() method" should "generate strings of the desired length" in {
     forAll (lengths) { (length: Int) =>
-      util.randomAlphanumericString(length)(new scala.util.Random) should have length length
+      util.random.randomAlphanumericString(length)(new scala.util.Random) should have length length
     }
   }
   it should "generate strings in the alphabet [abcdefghijklmnopqrstuvwxyz0123456789]*" in {
     forAll (lengths) { (length: Int) =>
-      util.randomAlphanumericString(length)(new scala.util.Random) should fullyMatch regex """[abcdefghijklmnopqrstuvwxyz0123456789]*"""
+      util.random.randomAlphanumericString(length)(new scala.util.Random) should fullyMatch regex """[abcdefghijklmnopqrstuvwxyz0123456789]*"""
     }
   }
   "The randomString() method" should "generate strings of the correct length" in {
     forAll (arbitrary[String], lengths) { (alphabet: String, length: Int) =>
       whenever (alphabet != "") {
-        util.randomString(alphabet)(length)(new scala.util.Random) should have length length
+        util.random.randomString(alphabet)(length)(new scala.util.Random) should have length length
       }
     }
   }
   it should "generate strings in the specified alphabet" in {
     forAll (arbitrary[String], lengths) { (alphabet: String, length: Int) =>
       whenever (alphabet != "") {
-        util.randomString(alphabet)(length)(new scala.util.Random) foreach {
+        util.random.randomString(alphabet)(length)(new scala.util.Random) foreach {
           (c: Char) => alphabet should contain (c)
         }
       }
@@ -42,12 +42,12 @@ class UtilSpec extends FlatSpec with GeneratorDrivenPropertyChecks with Matchers
   }
   "The randomJavaIdent() method" should "generate identifiers of the correct length" in {
     forAll (lengths) { (length: Int) =>
-      util.randomJavaIdent(length)(new scala.util.Random) should have length length
+      util.random.randomJavaIdent(length)(new scala.util.Random) should have length length
     }
   }
   it should "generate only valid Java identifiers" in {
     forAll (lengths) { (length: Int) =>
-      val ident = util.randomJavaIdent(length)(new scala.util.Random)
+      val ident = util.random.randomJavaIdent(length)(new scala.util.Random)
       Character isJavaIdentifierStart ( ident charAt 0 ) shouldBe true
       ident dropRight 1 foreach {
         Character isJavaIdentifierPart _ shouldBe true
