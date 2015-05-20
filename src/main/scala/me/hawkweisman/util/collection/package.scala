08df19808fb0d5c8d18044b6e74c4446d5041866
Hawk Weisman <hi@hawkweisman.me>
2015-05-20 12:52:24 -0400

    Add Collection package with RepeatableSeq

---
+++ src/main/scala/me/hawkweisman/util/collection/package.scala
@@ -0,0 +1,23 @@
+package me.hawkweisman.util
+/**
+ * Collection
+ * ==========
+ *
+ * Contains various personal utilities for working on collections.
+ */
+package object collection {
+
+  /**
+   * Enhances the standard library [[Seq]] type with the ability to stream repetitions.
+   * @param self a `Seq[A]`
+   * @tparam A the type of this [[Seq]]
+   */
+  implicit class RepeatableSeq[A](self: Seq[A]) {
+    /**
+     * Constructs a new infinite [[Stream]] that repeats this sequence of values.
+     * @return an infinite stream of repetitions of this sequence
+     */
+    def repeat(s: Seq[A]): Stream[A] = Stream.continually(self).flatten
+  }
+
+}
