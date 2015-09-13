bb95c97be0624d6a18a19a450654f9a7460090af
Hawk Weisman <hi@hawkweisman.me>
2015-09-13 15:22:26 -0400

    Now with working dot product

--- src/main/scala/me/hawkweisman/math/Matrix.scala
+++ src/main/scala/me/hawkweisman/math/Matrix.scala
@@ -2,6 +2,7 @@ package me.hawkweisman.math
 
 import Matrix.{ map2
               }
+import Function.tupled
 
 import scala.reflect.ClassTag
 
@@ -21,6 +22,8 @@ class Matrix[N: Numeric](protected val m: Array[Array[N]])
 
   lazy val columns: Int = m length
   lazy val rows: Int = m(0) length
+
+  type Vector = Array[N]
 
   @inline def isSameSizeAs(that: Matrix[N]): Boolean
     = this.columns == that.columns && this.rows == that.rows
@@ -45,11 +48,15 @@ class Matrix[N: Numeric](protected val m: Array[Array[N]])
   def times (scalar: N): Matrix[N]
     = new Matrix[N]( map2(m)(implicitly[Numeric[N]].times(_, scalar)) )
 
-  def cross_product (that: Matrix[N]): Matrix[N]
-    = ???
+  def times (that: Matrix[N]): Matrix[N]
+    = { require(this isSameSizeAs that,
+                "Cannot multiply matrices of unequal size")
+        val that_prime = that.m.transpose
+        new Matrix[N](this.m map {r ⇒ that_prime.map{c ⇒ dot(r, c)}})
+      }
 
-  def dot_product (that: Matrix[N]): Matrix[N]
-    = ???
+  def dot (a: Vector, b: Vector): N
+    = a zip b map tupled (implicitly[Numeric[N]].times(_, _)) sum
 
   def + (that: Matrix[N]): Matrix[N]
     = this plus that
@@ -58,6 +65,9 @@ class Matrix[N: Numeric](protected val m: Array[Array[N]])
     = this minus that
 
   def * (that: N): Matrix[N]
+    = this times that
+
+  def * (that: Matrix[N]): Matrix[N]
     = this times that
 
 }
