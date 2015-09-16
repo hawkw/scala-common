851d00cc9c50f357c4fc6d12e05ac663eae05de2
Hawk Weisman <hi@hawkweisman.me>
2015-09-15 21:49:52 -0400

    Made a bunch of Linear code slightly prettier

--- src/main/scala/me/hawkweisman/math/Linear.scala
+++ src/main/scala/me/hawkweisman/math/Linear.scala
@@ -310,9 +310,7 @@ extends Linear {
   private[this] def zap[N: Numeric : ClassTag]
                        (a: Matrix[N], b: Matrix[N])
                        (f: (N, N) ⇒ N): Matrix[N]
-    = a zip b map { case ((r1: Array[N], r2: Array[N])) ⇒
-        r1 zip r2 map tupled (f)
-      }
+    = for { (u, v) ← a zip b } yield u zip v map tupled (f)
 
   override def vectorAdd[N : Numeric : ClassTag]
                         (a: Vector[N], b: Vector[N]): Vector[N]
@@ -348,8 +346,7 @@ extends Linear {
     = { // currently, this is special cased because
         // unevenly sized vectors isn't done yet
         require(v.length == m(0).length)
-        m zip v map {  case ((row, s)) ⇒ row ^* s }
-
+        m zip v map tupled { (row, s) ⇒ row ^* s }
       }
 
   override def matrixScalarAdd[N: Numeric: ClassTag]
@@ -392,8 +389,9 @@ extends Linear {
                            (a: Matrix[N], b: Matrix[N]): Matrix[N]
     = { require( a.length == b(0).length
                , "X-cardinality of matrix A must equal y-cardinality of B." )
-        for (row ← a) yield for (col ← b transpose)
-          yield row * col
+        for (row ← a)
+        yield for (col ← b transpose)
+              yield row * col
       }
 
 }
@@ -422,9 +420,8 @@ extends Linear {
   @inline
   private[this] def zap[N: ClassTag](a: Matrix[N], b: Matrix[N])
                        (f: (N, N) ⇒ N): Matrix[N]
-    = a.par zip b.par map { case ((u, v)) ⇒
-        u.par zip v.par map tupled (f) toArray
-      } toArray
+    = (for{ (u, v) ← a.par zip b.par }
+       yield u.par zip v.par map tupled (f) toArray) toArray
 
   override def vectorAdd[N : Numeric : ClassTag]
                         (a: Vector[N], b: Vector[N]): Vector[N]
@@ -508,7 +505,7 @@ extends Linear {
     = { // currently, this is special cased because
         // unevenly sized vectors isn't done yet
         require(v.length == m(0).length)
-        m.par zip v.par map { case ((row, s)) ⇒ row ^* s } toArray
+        m.par zip v.par map tupled { (row, s) ⇒ row ^* s } toArray
     }
 
 }
@@ -522,9 +519,7 @@ extends Linear {
   private[this] def zap[N: Numeric : ClassTag]
                        (a: Matrix[N], b: Matrix[N])
                        (f: (N, N) ⇒ N): Matrix[N]
-    = a zip b map { case ((r1: Vector[N], r2: Vector[N])) ⇒
-      r1 zip r2 map tupled (f)
-    }
+    = for { (u, v) ← a zip b } yield u zip v map tupled (f)
 
   override def vectorAdd[N : Numeric : ClassTag]
                         (a: Vector[N], b: Vector[N]): Vector[N]
@@ -559,8 +554,8 @@ extends Linear {
                               (v: Vector[N], m: Matrix[N]): Matrix[N]
     = { // currently, this is special cased because
         // unevenly sized vectors isn't done yet
-        require(v.length == m(0).length)
-        m zip v map {  case ((row, s)) ⇒ row ^* s }
+        require(v.length == m.head.length)
+        m zip v map tupled { (row, s) ⇒ row ^* s }
       }
 
   override def matrixScalarAdd[N: Numeric: ClassTag]
@@ -582,22 +577,22 @@ extends Linear {
   override def dotProduct[N : Numeric : ClassTag]
                          (a: Vector[N], b: Vector[N]): N
     = { require( a.length == b.length
-      , "Cannot take dot product of vectors of unequal length" )
-      a zip b map tupled (_ * _) sum
-    }
+                , "Cannot take dot product of vectors of unequal length" )
+        a zip b map tupled (_ * _) sum
+      }
 
   override def matrixAdd[N : Numeric : ClassTag]
                         (a: Matrix[N], b: Matrix[N]): Matrix[N]
     = { require(a.length == b.length, "Cannot add matrices of unequal size")
-      zap(a, b)(_ + _)
-    }
+        zap(a, b)(_ + _)
+      }
 
   override def matrixSub[N : Numeric : ClassTag]
                         (a: Matrix[N], b: Matrix[N]): Matrix[N]
     = { require( a.length == b.length && a(0).length == b(0).length
-      , "Cannot subtract matrices of unequal size" )
-      zap(a, b)(_ - _)
-    }
+               , "Cannot subtract matrices of unequal size" )
+        zap(a, b)(_ - _)
+      }
 
   override def crossProduct[N : Numeric : ClassTag]
                            (a: Matrix[N], b: Matrix[N]): Matrix[N]
